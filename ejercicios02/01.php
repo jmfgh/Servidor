2b158f8c22c87c69ab4c32cc56a9e04d32e26beb
ALUMNO (A) <jmfgh@hotmail.com>
2020-10-05 15:12:33 +0200

    01

--- ejercicios02/01.php
+++ ejercicios02/01.php
@@ -1,4 +1,17 @@
 <?php
 
-   echo "hola";
+function elMayor($a, $b, &$c){
+    
+    if($a == $b){
+        $c = 0;
+    }elseif($a > $b){
+        $c = $a;
+    }else{
+        $c = $b;
+    }
+  return $c;
+}
+   echo elMayor(1, 1, $c)."</br>";
+   echo elMayor(2, 1, $c)."</br>";
+   echo elMayor(2, 4, $c);
 ?>
