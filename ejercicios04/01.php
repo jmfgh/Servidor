877b25dca48ebe5f9d8072ee6a68baf497b45957
jmfgh <jmfgh@hotmail.com>
2020-10-21 21:07:05 +0200

    primero

--- ejercicios04/01.php
+++ ejercicios04/01.php
@@ -1,10 +1,36 @@
-<?php
+<?php 
+
+function mostrarFormulario(){
+    echo "<html>
+            <body>
+            	<form method=\"POST\">
+            		<input type=\"text\" name=\"usu\"> Usuario
+            		<input type=\"password\" name=\"contra\"> Password
+            		<input type=\"submit\" value=\"Enviar\">
+            	</form>
+            </body>
+          </html>";
+}
 
     $registro = ["jose" => "1234", "alumno" => "alumno", "root" => "A2020"];
     
-    $usuario = $_POST[usu];
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){    
     
-    echo $usuario;
-
-
+        $usuario = $_REQUEST["usu"];
+        $clave = $_REQUEST["contra"];
+        
+        if (key_exists($usuario, $registro)) {
+            if($registro[$usuario] == $clave){
+                echo "Bienvenido ".$usuario;
+            }else{
+               echo "Password incorrecta.";
+               mostrarFormulario();
+            }
+        }else{
+            echo "Nombre de usuario incorrecto:";
+            mostrarFormulario();
+        }
+    }else{
+        mostrarFormulario();
+    }
 ?>
