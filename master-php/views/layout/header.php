375906556bbbdda1c4aca0830c7b1f54746b5e30
jmfgh <jmfgh@hotmail.com>
2021-02-13 13:06:51 +0100

    v4

--- master-php/views/layout/header.php
+++ master-php/views/layout/header.php
@@ -1,9 +1,14 @@
 <!DOCTYPE HTML>
 <html lang="es">
+<meta content="width=device-width, initial-scale=1" name="viewport" />
 	<head>
 		<meta charset="utf-8" />
 		<title>Tienda de Camisetas</title>
 		<link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<div id="container">
@@ -19,17 +24,25 @@
 
 			<!-- MENU -->
 			<?php $categorias = Utils::showCategorias(); ?>
-			<nav id="menu">
-				<ul>
-					<li>
-						<a href="<?=base_url?>">Inicio</a>
+
+			<nav class="navbar navbar-expand-lg" id="menu">
+
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#enlaces" aria-controls="enlaces" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon">Menú</span>
+				</button>
+			
+				<div class="collapse navbar-collapse" id="enlaces">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="<?=base_url?>">Inicio</a>
 					</li>
 					<?php while($cat = $categorias->fetch_object()): ?>
 						<li>
-							<a href="<?=base_url?>categoria/ver&id=<?=$cat->id?>"><?=$cat->nombre?></a>
+							<a class="nav-link" href="<?=base_url?>categoria/ver&id=<?=$cat->id?>"><?=$cat->nombre?></a>
 						</li>
 					<?php endwhile; ?>
 				</ul>
+				</div>
 			</nav>
 
 			<div id="content">
